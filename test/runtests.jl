c79f5597af239e4d34d12eae0735581eeaf34cd2
Yacine Al-najjar <yalnajja@gmail.com>
2026-08-17 22:45:26 +0200

    Modified runtests.jl to skip tests in test_HoldemEval_vs_PHEvaluator.jl if env variable PHEVALUATOR_LIB is not set.

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,7 @@ using Random
 using Combinatorics
 using HoldemEval
 
+Random.seed!(1234)
 
 include("PHEvaluatorWrapper.jl")
 using .PHEvaluatorWrapper
@@ -10,6 +11,17 @@ using .PHEvaluatorWrapper
 # Main test runner that executes each sub-test module
 @testset "HoldemEval.jl Suite" begin
     include("test_core.jl")
-    include("test_HoldemEval_vs_PHEvaluator.jl") # need the env variable PHEVALUATOR_LIB=/path/to/libpheval.so
+
+    cpp_lib_path = get(ENV, "PHEVALUATOR_LIB", "")
+
+    if !isempty(cpp_lib_path) && isfile(cpp_lib_path)
+        @testset "C++ PHEvaluator Comparison" begin
+            @info "Running C++ PHEvaluator comparison tests..."
+            include("test_HoldemEval_vs_PHEvaluator.jl") # need the env variable PHEVALUATOR_LIB=/path/to/libpheval.so
+        end
+    else
+        @info "Skipping C++ comparison tests (PHEVALUATOR_LIB not set or file not found)."
+    end
+    
     include("test_HoldemEval_vs_PokerHandEvaluator.jl")
 end
